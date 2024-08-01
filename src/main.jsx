46cf570277784bfa09d149c0fa78fe872df58183
RoaferTech <info@roafergroup.com>
2024-08-01 23:39:55 +0500

    redux resistors implemented to use storage

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store.js";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './slices/cartSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import cartReducer from './slices/cartSlice'; 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
 
+const persistedReducer = persistReducer(persistConfig, cartReducer);
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+export const persistor = persistStore(store);
